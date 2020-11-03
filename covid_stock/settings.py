3301258b7b949ce03b797d304db950ac036bd118
Erica Xia <29347801+EricaXia@users.noreply.github.com>
2020-11-02 18:32:14 -0800

    Update settings.py

--- covid_stock/settings.py
+++ covid_stock/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import djongo
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,11 +77,11 @@ WSGI_APPLICATION = 'covid_stock.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+    },
+    'news': {
         'ENGINE': 'djongo',
         'NAME': 'news',
         'HOST': 'mongodb+srv://dsci551:123@cluster0.mhniy.gcp.mongodb.net/news?retryWrites=true&w=majority',

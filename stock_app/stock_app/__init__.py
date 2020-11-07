2697eba14b43f5683d5fff891772f8884b9d008b
Erica Xia <29347801+EricaXia@users.noreply.github.com>
2020-11-06 17:05:08 -0800

    Update __init__.py

--- stock_app/stock_app/__init__.py
+++ stock_app/stock_app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask 
 from flask_bootstrap import Bootstrap
-from .extensions import mongo
+from .extensions import mongo, sql_alchemy
 from .main import main
 
+
 def create_app(config_object='stock_app.settings'):
+    ## initialize Flask app
     app = Flask(__name__)
+    
+    ## make Bootstrap templates
     Bootstrap(app)
     app.config.from_object(config_object)
+
+    ## bind the db handlers to the app
     mongo.init_app(app)
+    sql_alchemy.init_app(app)
+
     app.register_blueprint(main)
     return app
